c22a94ac85d927cbf5c79cdb14af3acec19c065b
Unknown <natsik94@mail.ru>
2018-06-17 23:54:35 +0300

    create some board logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+//material imports
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppComponent } from './app.component';
-import { BoardComponent } from './board/board.component';
+import { BoardComponent, AddNewModal } from './board/board.component';
 import { TaskComponent } from './task/task.component';
 
 
@@ -12,14 +22,26 @@ import { TaskComponent } from './task/task.component';
   declarations: [
     AppComponent,
     BoardComponent,
-    TaskComponent
+    TaskComponent,
+    AddNewModal
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    FormsModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AddNewModal,
+],
 })
 export class AppModule { }
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.less']
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
-  ngOnInit() {
+
+  showForm() {
+    let dialogRef = this.dialog.open(AddNewModal, {
+      width: '250px',
+      data: { name: 'Add new task' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed with result', result);
+    });
+  }
+
+}
+
+@Component({
+  selector: 'add-new',
+  template: `<h1 mat-dialog-title>{{data.name}}</h1>
+              <div mat-dialog-content>
+                <p>What's your task?</p>
+                <mat-form-field>
+                  <input matInput [(ngModel)]="data.task">
+                </mat-form-field>
+              </div>
+              <div mat-dialog-actions>
+                <button mat-button (click)="onNoClick()">No Thanks</button>
+                <button mat-button [mat-dialog-close]="data.task" cdkFocusInitial>Ok</button>
+              </div>`
+})
+export class AddNewModal {
+
+  constructor(
+    public dialogRef: MatDialogRef<AddNewModal>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
